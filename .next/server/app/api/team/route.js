835edd47d44eb5a87d77cc8de9d0dcc5ce9689a4
franciosse <franciosse@gmail.com>
2025-08-13 11:50:07 +0200

    Remove .next directory from version control and update .gitignore

--- .next/server/app/api/team/route.js
+++
@@ -1,9 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/team/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/46988_next_ec4373e3._.js");
-runtime.loadChunk("server/chunks/8f779_drizzle-orm_28270606._.js");
-runtime.loadChunk("server/chunks/node_modules__pnpm_e9e8d0a2._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__a394d2fd._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/team/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.4.0-canary.47_react-dom@19.1.0_react@19.1.0__react@19.1.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/team/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.4.0-canary.47_react-dom@19.1.0_react@19.1.0__react@19.1.0/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/team/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fTvPIYCZMVrG3dWbtBUhgMaouz0TN/A37fSXGlIHMBA=\"\n}"
